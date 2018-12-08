ada2eecdaf0a32a514d196b236829bcd47f2e00a
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2018-12-08 21:01:51 +0100

    [day08] nim part 2 completed

--- nim/day08.nim
+++ nim/day08.nim
@@ -52,43 +52,18 @@ proc sumMetas(node: Node): int =
 
 echo root.sumMetas()
 
+proc value(node: Node): int =
+  if node.numChildren == 0:
+    return sum(node.metas)
+  for i in node.metas:
+    if 0 < i and i <= node.numChildren:
+      result += node.children[i-1].value()
+
+echo root.value()
+
 # real case:
 const input = readFile("./inputs/08.txt")
 sequence = process(input)
 root = initNode(sequence)
 echo root.sumMetas()
-#[
-
-    def sum_metas(self):
-        sum_metas = sum(self.meta)
-        for child in self.children:
-            sum_metas += child.sum_metas()
-        return sum_metas
-    # def __iter__(self):
-    #     return self
-    
-    # def __next__(self):
-    #     yield self
-    #     for child in self.children:
-    #         for node in child:
-    #             yield node
-
-sequence = test_input.split()
-sequence.reverse()
-sequence = list(map(lambda x: int(x), sequence))
-root = node(sequence)
-print(root)
-
-# summing all metas
-print(root.sum_metas())
-
-# real case
-# real case:
-with open('./inputs/08.txt') as f:
-    input = f.read()
-sequence = input.split()
-sequence.reverse()
-sequence = list(map(lambda x: int(x), sequence))
-root = node(sequence)
-print(root.sum_metas())
-]#
+echo root.value()
